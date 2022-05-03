351a6641b05bf8787bf5ad1fc7e9dc55e6c096fe
Hedi23 <ajimi-hedi@hotmail.fr>
2022-05-03 11:57:54 +0200

    changed ghost init file

--- shell/ghost_init.sh
+++ shell/ghost_init.sh
@@ -11,38 +11,40 @@ LB_HOSTNAME=${LB_HOSTNAME}
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
     # Update packages
-    apt-get update && sudo apt-get upgrade -y
+    sudo apt-get update && sudo apt-get upgrade -y
 
     # Install Nginx
-    apt-get install -y nginx 
+    sudo apt-get install -y nginx 
 
     # Increase the server_names_hash_bucket_size to 128 in order to accept long domain names
     echo "server_names_hash_bucket_size 128;" | sudo tee /etc/nginx/conf.d/server_names_hash_bucket_size.conf
 
     # Add the NodeSource APT repository for Node 12
-    curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash
+    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash
 
     # Install Node.js && npm
-    apt-get install -y nodejs
-    npm install npm@latest -g
+    sudo apt-get install -y nodejs
+    sudo npm install npm@latest -g
 
     # Install Ghost-CLI
-    npm install ghost-cli@latest -g
+    sudo npm install ghost-cli@latest -g
 
     # Give permission to ubuntu user, create directory 
-    chown -R ubuntu:ubuntu /var/www/
+    sudo chown -R ubuntu:ubuntu /var/www/
     sudo -u ubuntu mkdir -p /var/www/blog && cd /var/www/blog
+
+    sudo chown -R ubuntu:ubuntu /home/ssm-user/
 
     # Install Ghost, cannot be run via root (user data default)
     sudo -u ubuntu ghost install \
         # --url       "${URL}" \
         # --admin-url "${ADMIN_URL}" \
-        --url       "${LB_HOSTNAME}" \
-        --admin-url "${LB_HOSTNAME}" \
+        --url       "http://${LB_HOSTNAME}" \
+        --admin-url "http://${LB_HOSTNAME}" \
         --db        "mysql" \
         --dbhost    "${DB_HOSTNAME}" \
         --dbuser    "${DB_USERNAME}" \
         --dbpass    "${DB_PASSWORD}" \
         --dbname    "${DB_NAME}" \
         --process systemd \
-        --no-prompt 
+        --no-prompt
